3b8bdbc2e7f1dc448f32a0c8d4c483009202c1fc
Ajitesh Kumar <ajinest6@gmail.com>
2026-04-04 12:27:08 -0400

    add controller usb? avoid conflict idk if this works tho

--- daemon/vms/create.go
+++ daemon/vms/create.go
@@ -102,6 +102,10 @@ func buildDomainXML(c *server.VMConfig, isoPath, diskPath, bridge string) string
   <on_crash>destroy</on_crash>
 
   <devices>
+	<controller type='usb' index='0' model='qemu-xhci'>
+      <address type='pci' domain='0x0000' bus='0x02' slot='0x00' function='0x0'/>
+    </controller>
+
     <!-- Primary disk (persistent, writable) -->
     <disk type='file' device='disk'>
       <driver name='qemu' type='qcow2' cache='writeback'/>
