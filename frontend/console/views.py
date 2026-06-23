e363f0fa86acd446430e61219fd85beb144a9df6
Ajitesh Kumar <ajinest6@gmail.com>
2026-06-23 01:20:44 -0400

    machines, migrate and ignore db

--- frontend/console/views.py
+++ frontend/console/views.py
@@ -3,8 +3,8 @@ from .models import Machine
 
 # Create your views here.
 def index(request):
-    return render(request, 'console/index.html')
+    return render(request, 'index.html')
 
 def list_machines(request):
     machines = Machine.objects.all()
-    return render(request, 'console/machines.html', {'machines': machines})
+    return render(request, 'machines.html', {'machines': machines})
